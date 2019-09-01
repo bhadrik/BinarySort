a38b5310feb415650e5bee6c713e74cd8732e182
Bhadrik Jagad <jagadbhadrik4@gmail.com>
2019-09-01 23:38:56 +0530

    Update BinerySort.cpp

--- BinerySort.cpp
+++ BinerySort.cpp
@@ -3,7 +3,6 @@
 #include<iostream>
 #define n 5
 using namespace std;
-//int n=5;
 int globalArray[5];
 short ptr=0;
 void my_sort(int*, short, short);
@@ -11,17 +10,7 @@ void my_sort(int*, short, short);
 int main()
 {
 	int arr[n]={50,83,95,14,63};
-	/*int num,ii=0;
-	int *arr=(int*)malloc(0);
-	cout<<"Enter numbers u want to sort"<<endl<<"Enter -1 if complete"<<endl;
-	while(num!=-1){
-		cin>>num;
-		realloc(arr,sizeof(int));
-		arr[ii]=num;
-		ii++;
-	}
-	n=ii;
-	*/
+	
 	for(short i=6;i>=0;i--){
 		my_sort(arr,i,n);
 		for(int i=0;i<n;i++){
@@ -41,21 +30,17 @@ void my_sort(int *arr,short bitNo, short length){
 	short newLength = 0;
 	bool gone=0;
 
-	cout<<"length :"<<length<<endl;
-	cout<<"bitNo :"<<bitNo<<endl;
-
 	if(bitNo == 0){
-		cout<<"bitNo == 0\n";
 		return;
 	}
 
 	if(length == 1 ){
-			globalArray[ptr]=arr[0];
-			ptr++;
-			cout<<"leng == 1\n";
-			return ;
+		globalArray[ptr]=arr[0];
+		ptr++;
+		return;
 	}
-	if(length==2){
+	
+	if(length == 2){
 		globalArray[ptr]=arr[1]>arr[0]?arr[1]:arr[0];
 		ptr++;
 		globalArray[ptr]=arr[1]>arr[0]?arr[0]:arr[1];
@@ -67,25 +52,14 @@ void my_sort(int *arr,short bitNo, short length){
 				realloc(subarray,sizeof(int));
 				subarray[newLength] = arr[i];
 				newLength++;
-				cout<<"Yes"<<endl;
 				gone=1;
 			}
 		}
 	}
-	cout<<subarray[0]<<" + "<<subarray[1]<<endl;
-	cout<<"BEF:";
-	for(short i=0;i<n;i++)
-		cout<<globalArray[i]<<" ";
-	cout<<endl;
 	if(length>2){
 		if(!gone)
 			my_sort(arr,bitNo-1,length);
 		else
 			my_sort(subarray,bitNo-1,newLength);
 	}
-
-	cout<<"AFT:";
-	for(short i=0;i<n;i++)
-		cout<<globalArray[i]<<" ";
-	cout<<endl;
 }
