e1a98af596c937daa1cd829f9dda6ee82ae7fd0d
Bhadrik Jagad <jagadbhadrik4@gmail.com>
2020-09-12 14:56:55 +0530

    New method
    Added little description about bitNo

--- BinarySort.c
+++ BinarySort.c
@@ -10,9 +10,9 @@ void swap(int* data, int i1, int i2) {
 }
 
 //Print the content of given array
-void displayData(int* data) {
+void displayData(int *data) {
 	int i = 0;
-	printf("%d", data[0]);
+	printf("%d",data[0]);
 	for (i = 1; i < N; i++)
 		printf(", %d", data[i]);
 	printf("\n");
@@ -21,7 +21,7 @@ void displayData(int* data) {
 //Perform binery sort
 void binarySort(int* data, int bitNo, int* lowerBound, int* upperBound) {
 
-	//Start form given bit number in "bitNo"
+	//Start form given bit number in "bitNo" and goes into a loop until it becomes 0
 	if (bitNo == 0) return;
 
 	int l_backup = *lowerBound, u_backup = *upperBound, i;
@@ -49,11 +49,31 @@ void binarySort(int* data, int bitNo, int* lowerBound, int* upperBound) {
 
 int main() {
 	int data[N] = { 70, 80, 30, 60, 50, 90, 20, 40, 90, 15 };
-	int lowerBound = 0, upperBound = N - 1;
+	int lowerBound = 0, upperBound = N - 1, bitNo = 6;
 
 	displayData(data);
 
-	binarySort(data, 6, &lowerBound, &upperBound);
+	/*
+	* BitNo is most importent thing in this algorithm because it affect the performace most.
+	* Here I have choosen 6 as bitNo because there is not a single bit 1 after 6th bit in any number.
+	* 
+	*                   <- bitNo
+	*     10 9 8 7 6 5 4 3 2 1 0
+	* 70:  0 0 0 0 1 0 0 0 1 1 0
+	* 80:  0 0 0 0 1 0 1 0 0 0 0
+	* 30:  0 0 0 0 0 0 1 1 1 1 0
+	* 60:  0 0 0 0 0 1 1 1 1 0 0
+	* 50:  0 0 0 0 0 1 1 0 0 1 0 
+	* 90:  0 0 0 0 1 0 1 1 0 1 0
+	* 20:  0 0 0 0 0 0 1 0 1 0 0
+	* 40:  0 0 0 0 0 1 0 1 0 0 0
+	* 90:  0 0 0 0 1 0 1 1 0 1 0
+	* 15:  0 0 0 0 0 0 0 1 1 1 1
+	* 
+	* So either you have to determine bitNo from your input data manuly or can
+	* choose maximum number of bits in your data type like int(4byte)-> 32bit.
+	*/
+	binarySort(data, bitNo, &lowerBound, &upperBound);
 
 	displayData(data);
 
